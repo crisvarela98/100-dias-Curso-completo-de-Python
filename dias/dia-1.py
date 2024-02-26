b08463fec4d5539a3d20582ef08d9adb6143b77d
Cristian Varela <95387609+crisvarela98@users.noreply.github.com>
2024-02-26 17:45:49 +0000

    Initial commit

---
+++ dias/dia-1.py
@@ -0,0 +1,7 @@
+print("Cris")
+print("Febrero 26, 2024")
+print("""¡Me inscribo en el desafío de 100 días de Python de Replit!
+Me aseguraré de dedicar algo de tiempo todos los días a codificar, durante un mínimo de 10 minutos al día.
+Usaré Replit ¡No hay excusas para no codificar!
+Me siento genial 😊""")
+print("podes seguir mi progreso en replit.com/@varelacristiano")
